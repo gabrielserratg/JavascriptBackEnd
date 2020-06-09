00c6ff0c48a630cba528a7d00c919a3254a0b8fd
root <root@kali>
2020-06-09 05:55:14 +0000

    gerador de hash para senha

--- src/server.js
+++ src/server.js
@@ -13,18 +13,26 @@ app.get('/users', (req, res) => {
 
 app.post('/users', async(req, res) => {
     try {
-        const salt = bcrypt.genSalt()
-        const hashedpassword = await bcrypt.hash(req.body.password, salt)
-        console.log(salt)
-        console.log(hashedpassword)
-        const user = { name: req.body.name, password: hashedpassword }
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        console.log("Password in Hash: " + hashedPassword)
+        const user = { name: req.body.name, password: hashedPassword }
         users.push(user)
         res.status(201).send()
     } catch {
         res.status(500).send()
     }
-
 })
 
+app.post('/users/login', async(req, res) => {
+    const user = users.find(user => user.name = req.body.name)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+    }
+    try {
+        await bcrypt.compare(req.body.password, user.password)
+    } catch {
+        res.status(500).send()
+    }
+})
 
 app.listen(3000)
