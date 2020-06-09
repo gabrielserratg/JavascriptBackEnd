1a6c3b6b1f6c17f9ff8ee303fdaf49026a03dd3e
root <root@kali>
2020-06-09 05:25:51 +0000

    no ponto

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+
+const users = [{ name: 'Name' }]
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+
+
+app.listen(3000)
