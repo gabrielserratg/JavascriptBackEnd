0474958e25e8fe71ba01edbb83671c7b067d712d
root <root@kali>
2020-06-09 05:32:37 +0000

    primeira parte do preset

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json)
 
-const users = [{ name: 'Name' }]
+const users = []
+
 
 app.get('/users', (req, res) => {
     res.json(users)
 })
 
+app.post('/users', (req, res) => {
+    const user = { name: req.body.name, password: req.body.password }
+    users.push(user)
+})
 
 
 app.listen(3000)
