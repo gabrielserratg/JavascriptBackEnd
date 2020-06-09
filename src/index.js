eeae019d2464a052fe6744d13e4b874b4ceed3fc
root <root@kali>
2020-06-09 03:08:34 +0000

    index.js update v2 testing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const router = express.Router();
 
-app.get('/', function(req, res) {
-    res.sendfile('index.html')
-})
 
-console.log("iniciado..")
-app.listen(80)
+router.get('/', function(req, res) {
+    res.sendfile(path.join(__dirname + '/index.html'));
+});
+
+router.get('/about', function(req, res) {
+    res.sendfile(path.join(__dirname + '/about.html'));
+});
+
+router.get('/contact', function(req, res) {
+    res.sendfile(path.join(__dirname + '/contact.html'));
+});
+
+
+app.use('/', router);
+app.listen(process.env.port || 80);
+
+console.log('Running at Port 80')
